1a0fea5961880199bc1f0ab5bc4b59e26acad709
marcos.moura <marcos.moura@rentcars.com>
2020-04-03 12:15:33 -0300

    add environment variable

--- app/sender.py
+++ app/sender.py
@@ -1,6 +1,7 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
 
 
@@ -8,9 +9,16 @@ class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres host=db'
-        self.conn = psycopg2.connect(DSN)
+
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+        
+        db_host = os.getenv('DB_HOST', 'db')
+        db_name = os.getenv('DB_NAME', 'sender')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_password = os.getenv('DB_PASSWORD', 'postgres')    
+        dsn = f'dbname={db_name} user={db_user} password={db_password} host={db_host}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
@@ -35,4 +43,4 @@ class Sender(Bottle):
 
 if __name__ == '__main__':
     sender = Sender()
-    sender.run(host='0.0.0.0', port=8000, debug=True)
+    sender.run(host='0.0.0.0', port=8080, debug=True)
